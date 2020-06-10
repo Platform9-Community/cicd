3a8306defafb69243a4a5c4f7e6d6b0606bb9084
pfray <sail50638@gmail.com>
2020-06-10 09:09:15 -0400

    moving react-app01 to KoolKubernets Org

---
+++ react-app01/build/precache-manifest.c090542600f4afc278af59f656cabb30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05384b711acc7fc4d253d71a036f7bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131c01ffb98421a4daf7",
+    "url": "/static/css/main.b6b0e01b.chunk.css"
+  },
+  {
+    "revision": "a57f79f23f0c304a0078",
+    "url": "/static/js/2.e3fa3ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3fa3ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131c01ffb98421a4daf7",
+    "url": "/static/js/main.74b2a9e4.chunk.js"
+  },
+  {
+    "revision": "53609fdbe8f05d742c83",
+    "url": "/static/js/runtime-main.c36de252.js"
+  },
+  {
+    "revision": "23f3a31708c33364e80966877cb8ea4b",
+    "url": "/static/media/k8logo.23f3a317.png"
+  }
+]);
