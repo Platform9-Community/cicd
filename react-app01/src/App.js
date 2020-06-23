482fe5d9b300269b1b8f2444751ac4bb0cb477ac
pfray <sail50638@gmail.com>
2020-06-23 10:20:40 -0400

    demo

--- react-app01/src/App.js
+++ react-app01/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Howdie pf9
+          Howdie world
         </p>
         <a
           className="App-link"
