7ee137ba3c9b9229e36ba002bcb96729402c3a29
snimje <surendra@platform9.com>
2020-06-23 19:46:40 +0530

    Update App.js

--- jenkins/webapp01/src/App.js
+++ jenkins/webapp01/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Howdie everyone!
+          Howdie everyone
         </p>
         <a
           className="App-link"
