6a31804d5bc92fef81f919a2af44ec7a7fc8382e
pfray <sail50638@gmail.com>
2020-06-23 22:05:50 -0400

    howdy pf9

--- react-app01/src/App.js
+++ react-app01/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Howdie world
+          Howdie pf9
         </p>
         <a
           className="App-link"
