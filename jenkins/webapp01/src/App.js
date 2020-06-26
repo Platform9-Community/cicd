cb9768180f3df9aa187cd2e40f51f91d508651d5
snimje <surendra@platform9.com>
2020-06-26 18:11:50 +0530

    Update App.js

--- jenkins/webapp01/src/App.js
+++ jenkins/webapp01/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Howdie PF9
+          Howdie PF9.
         </p>
         <a
           className="App-link"
